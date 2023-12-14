9fb719f045394767bc33a2a819c72bdcd41e2443
Alicja <alicjakruszewicz3@gmail.com>
2023-12-14 16:57:33 +0100

    Modify testSetAndGet test.

--- CedeczkoApp/src/test/java/com/cedeczko/app/logic/util/TestSearchParams.java
+++ CedeczkoApp/src/test/java/com/cedeczko/app/logic/util/TestSearchParams.java
@@ -15,15 +15,17 @@ public class TestSearchParams {
 
     @Test
     public void testSetAndGet() {
-        SearchParams searchParams = new SearchParams(3);
+        SearchParams searchParams = new SearchParams(4);
 
         searchParams.set(0, "param1");
         searchParams.set(1, "param2");
         searchParams.set(2, "param3");
+        searchParams.set(2, "param4");
 
         assertEquals("param1", searchParams.get(0));
         assertEquals("param2", searchParams.get(1));
         assertEquals("param3", searchParams.get(2));
+        assertEquals("param4", searchParams.get(3));
     }
 
     @Test
