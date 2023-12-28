d127268f9edc5ae46471018c449e8d76ddb0df1c
amichals <01178565@pw.edu.pl>
2023-12-28 13:47:56 +0100

    Update BasketWindow.java

--- CedeczkoApp/src/main/java/com/cedeczko/app/windows/BasketWindow.java
+++ CedeczkoApp/src/main/java/com/cedeczko/app/windows/BasketWindow.java
@@ -43,7 +43,7 @@ public class BasketWindow extends JFrame {
         up_panel.setPreferredSize(new Dimension(wide, upper_high));
         // przycisk powrót
         JButton return_button = new JButton("<--- Powrót");
-        return_button.addActionListener(e -> new ProductWindow(this));
+        return_button.addActionListener(e -> new MovieSearchWindow(this));
         up_panel.add(return_button);
 
         add(up_panel, BorderLayout.NORTH);
