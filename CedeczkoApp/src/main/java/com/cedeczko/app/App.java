cf1e49c62c141f02586fa9788a1be6726c2208fd
Alicja <alicjakruszewicz3@gmail.com>
2024-01-21 19:19:38 +0100

    Add new LookAndFeel theme

--- CedeczkoApp/src/main/java/com/cedeczko/app/App.java
+++ CedeczkoApp/src/main/java/com/cedeczko/app/App.java
@@ -1,17 +1,25 @@
 package com.cedeczko.app;
 
+import ch.randelshofer.quaqua.QuaquaLookAndFeel;
 import com.cedeczko.app.windows.BasketWindow;
 import com.cedeczko.app.windows.LoaderWindow.LoaderWindow;
 import com.cedeczko.app.windows.MainWindow;
 import com.cedeczko.app.windows.MovieSearchWindow.MovieSearchWindow;
+import mdlaf.MaterialLookAndFeel;
 
 import javax.swing.*;
+import javax.swing.plaf.metal.*;
 import java.awt.*;
 
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             LoaderWindow loaderWindow = new LoaderWindow();
+            try {
+                UIManager.setLookAndFeel(new QuaquaLookAndFeel());
+            } catch (UnsupportedLookAndFeelException e) {
+                throw new RuntimeException(e);
+            }
             MovieSearchWindow movieSearchWindow = new MovieSearchWindow(loaderWindow);
 //            MainWindow mainWindow = new MainWindow(basketWindow);
         });
